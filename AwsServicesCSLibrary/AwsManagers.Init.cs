dd6087da3cde39c7d4b72a647e341f68d66c60b7
SanadaSeiji <yuxu@tcd.ie>
2020-06-09 16:50:16 +0100

    split AwsManagers into partial classes

---
+++ AwsServicesCSLibrary/AwsManagers.Init.cs
@@ -0,0 +1,26 @@
+﻿using Amazon.Extensions.NETCore.Setup;
+using AwsServicesCSLibrary.Interfaces;
+using Amazon.S3;
+using Amazon.SQS;
+
+namespace AwsServicesCSLibrary
+{
+    public partial class AwsManagers:IAwsManagers
+    {
+        // in constructor set up client for every services?
+        private readonly IAmazonS3 s3Client;
+        private readonly string bucketName;
+
+        private readonly IAmazonSQS sqs;
+        private readonly string queueUrl;
+
+        public AwsManagers(AWSOptions options, AwsConfig config)
+        {
+            s3Client = options.CreateServiceClient<IAmazonS3>();
+            bucketName = config.Bucket;
+
+            sqs = options.CreateServiceClient<IAmazonSQS>();
+            queueUrl = config.Queue;
+        }
+    }
+}
---
+++ AwsServicesCSLibrary/AwsManagers.Sqs.cs
@@ -0,0 +1,12 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using System.Threading.Tasks;
+using AwsServicesCSLibrary.Interfaces;
+using Amazon.Extensions.NETCore.Setup;
+
+namespace AwsServicesCSLibrary
+{
+    public partial class AwsManagers:IAwsManagers
+    {
+    }
+}
